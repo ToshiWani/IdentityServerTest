d16e81d74dd7a71cdac034e555efbda92d4b471c
tkunisada <tkunisada@infragistics.com>
2018-06-08 18:11:33 -0400

    initial commit

---
+++ IdentityServerTest.OAuth/Configuration/InMemoryConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerTest.OAuth.Configuration
+{
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("identity_server_test", "Identity Server Test")
+            };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "identity_server_test",
+                    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "identity_server_test" }
+                }
+            };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[]
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "test@sharklasers.com",
+                    Password = "password"
+                }
+            };
+        }
+
+    }
+}
---
+++ IdentityServerTest.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityServerTest.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
